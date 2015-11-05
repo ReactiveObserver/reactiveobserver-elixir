182001681927cebd608d922f4395697ccfd1c1cb
Michał Łaszczewski <michal@cloudforge.pl>
2015-11-05 12:29:28 +0100

    sockjs_handler abstraction

--- lib/reactive_observer.ex
+++ lib/reactive_observer.ex
@@ -1,3 +1,8 @@
 defmodule ReactiveObserver do
   require Logger
+
+  def sockjs_handler(path,api) do
+    state=:sockjs_handler.init_state(<<"/sockjs">>, &ReactiveObserver.SockJsHandler.handle/3, {:new_connection, api}, [])
+    {path<>"/[...]", :sockjs_cowboy_handler, state}
+  end
 end
